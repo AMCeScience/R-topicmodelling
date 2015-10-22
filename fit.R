dca4d2ebe7cde0d6e8b9710e43d3631c7bd370f8
Flythe <Flythe1@gmail.com>
2015-10-22 15:56:08 +0200

    split into functions and files, preprocessing steps done, topic estimation test in main.R file

---
+++ fit.R
@@ -0,0 +1,86 @@
+K <- 5 # Number of topics
+G <- 200 # Iterations
+alpha <- 0.01 # Document to topic distribution, chance that a document belongs to a certain topic
+eta <- 0.01 # Topic to term distribution, chance that a certain term belongs to a topic (eg. topic 1 has a 0.05 chance of containing term X while topic 2 has a 0.0001 chance)
+
+library(lda)
+library(LDAvis)
+library(topicmodels)
+library(dplyr)
+
+LDASimulation <- function(corpus) {
+  # Convert tm corpus to document list, both LDA and topicmodels methods should use the TM to parse the original text
+  # This line below can then convert for the LDA while topicmodels uses the TermDocumentMatrix
+  # https://stackoverflow.com/questions/21148049/r-topic-modeling-lda-command-lexicalize-giving-unexpected-results
+  LDADocuments <- as.list(data.frame(text = unlist(sapply(corpus, `[`, "content")), stringsAsFactors = F))[[1]]
+  
+  # Get terms by splitting the documents on whitespace
+  usedTerms <- table(unlist(strsplit(LDADocuments, "[[:space:]]+")))
+  
+  # Lexicalize the documents
+  LDADocuments <- lexicalize(LDADocuments, vocab = names(usedTerms))
+  
+  # Get the sum of terms per document
+  tokensPerDoc <- sapply(LDADocuments, function(x) sum(x[2, ]))
+  
+  # Run LDA
+  LDAData <- lda.collapsed.gibbs.sampler(documents = LDADocuments, K = K, vocab = names(usedTerms), num.iterations = G, alpha = alpha, eta = eta, compute.log.likelihood = TRUE)
+  
+  # Document to topic distribution estimate
+  # Matrix where each column contains the probability distribution over topics for 1 document (1 cell is a probability of topic x for document y)
+  theta <- t(apply(LDAData$document_sums + alpha, 2, function(x) x/sum(x)))
+  # Topic to term distribution estimate
+  # Matrix where each column contains the probability distribution over words for 1 topic (1 cell is a probability of word x for topic y)
+  phi <- t(apply(t(LDAData$topics) + eta, 2, function(x) x/sum(x)))
+  
+  # Return a list of the LDA run, used terms, term frequency, and terms per document
+  return(list(LDAData = LDAData, usedTerms = names(usedTerms), termFrequency = as.integer(usedTerms), tokensPerDoc = tokensPerDoc, phi = phi, theta = theta))
+}
+
+TmLDASimulation <- function(corpus) {
+  dtm <- DocumentTermMatrix(corpus)
+  
+  LDAData <- LDA(x = dtm, k = K, method = "Gibbs")
+  
+  # Solution to translate between topicmodels and LDAVis: http://www.r-bloggers.com/a-link-between-topicmodels-lda-and-ldavis/
+  # Document to topic distribution estimate
+  # Matrix where each column contains the probability distribution over topics for 1 document (1 cell is a probability of topic x for document y)
+  theta <- posterior(LDAData)$topics %>% as.matrix
+  # Topic to term distribution estimate
+  # Matrix where each column contains the probability distribution over words for 1 topic (1 cell is a probability of word x for topic y)
+  phi <- posterior(LDAData)$terms %>% as.matrix
+  
+  usedTerms <- colnames(phi)
+  
+  tokensPerDoc <- vector()
+  
+  for (i in 1:length(corpus)) {
+    tokensPerDoc <- c(tokensPerDoc, stri_count(paste(corpus[[i]]$content, collapse = ' '), regex = '\\S+'))
+  }
+  
+  #tempFrequency <- inspect(dtm)
+  #termFrequency <- data.frame(ST = colnames(dtm), Freq = colSums(dtm))
+  termFrequency = colSums(inspect(dtm))
+  
+  return(list(LDAData = LDAData, usedTerms = usedTerms, termFrequency = termFrequency, tokensPerDoc = tokensPerDoc, phi = phi, theta = theta))
+}
+
+visualise <- function(LDAData, usedTerms, termFrequency, tokensPerDoc, phi, theta, outputFolder) {
+  # Put all data into a single list
+  json.raw <- list(phi = phi, theta = theta, doc.length = tokensPerDoc, vocab = usedTerms, term.frequency = termFrequency)
+  
+  # Create JSON format data
+  json.data <- createJSON(phi = json.raw$phi, theta = json.raw$theta, doc.length = json.raw$doc.length, vocab = json.raw$vocab, term.frequency = json.raw$term.frequency)
+  
+  # Remove the directory (throws error otherwise)
+  unlink(outputFolder, recursive = TRUE)
+  
+  # Create browser scripts
+  serVis(json.data, out.dir = outputFolder, open.browser = FALSE)
+}
+
+lda <- LDASimulation(cleanCorpus)
+visualise(lda$LDAData, lda$usedTerms, lda$termFrequency, lda$tokensPerDoc, lda$phi, lda$theta, "lda_vis")
+
+#lda <- TmLDASimulation(cleanCorpus)
+#visualise(lda$LDAData, lda$usedTerms, lda$termFrequency, lda$tokensPerDoc, lda$phi, lda$theta, "tm_vis")
---
+++ main.R
@@ -0,0 +1,70 @@
+# Clear workspace
+#rm(list = ls())
+
+# Set workspace to folder where articles.csv is placed
+setwd('~/workspace/R')
+
+if (!exists("cleanCorpus")) source("preprocessing.R")
+
+library(topicmodels)
+
+# https://github.com/cpsievert/xkcd/tree/master/code
+# https://github.com/cpsievert/xkcd/blob/master/code/03-fitLDA.R 
+# -------------------------------------------------------------------------
+# General idea: 
+#  (1) Split documents into training/test sets.
+#  (2) Fit models (varying by the number of topics) to the training set.
+#  (3) Compute the perplexity (log-likelihood of the held-out test set) for
+#   each fitted model.
+# -----------------------------------------------------------------------
+
+dtm <- DocumentTermMatrix(cleanCorpus)
+
+# If we have a term with zero frequency in the training data, that will
+# pose problems for LDAvis (can't divide by zero!).
+# This function will keep searching for a train/test split that 
+# yields a non-zero frequency for every term in the training data.
+split_dtm <- function(dtm, perc = 0.1) {
+  n <- dtm$nrow
+  idx <- sample(n, size = floor(n * perc))
+  
+  dtm_train <- dtm[-idx, ]
+  dtm_test <- dtm[idx, ]
+  
+  # termFreqs <- colSums(as.matrix(dtm_train))
+  
+  return(list(dtm_train = dtm_train, dtm_test = dtm_test))
+#   if (any(termFreqs == 0)) {
+#     split_dtm(dtm)
+#   } else {
+#     return(list(dtm_train = dtm_train, dtm_test = dtm_test))
+#   }
+}
+
+set.seed(43523) # for reproducibility
+dtms <- split_dtm(dtm)
+
+dtm_train <- dtms$dtm_train
+dtm_test <- dtms$dtm_test
+
+# save some summary statistics that we'll need for LDAvis
+termFreqs <- colSums(as.matrix(dtm_train))
+#stopifnot(!any(termFreqs == 0))
+saveRDS(termFreqs, "data/termFreqs.rds")
+
+docLens <- rowSums(as.matrix(dtm_train))
+stopifnot(!any(docLens == 0))
+saveRDS(docLens, "data/docLens.rds")
+
+# fit a bunch of models -- varying the number of topics
+# section 2.4 of http://www.jstatsoft.org/v40/i13/paper
+# has a nice, concise overview of model selection for LDA
+ks <- seq(2, 25, by = 2)
+models <- lapply(ks, function(k) LDA(dtm_train, k, method = "Gibbs", control = list(alpha = 1/k, delta = 0.1, burnin = 100, iter = 100, keep = 50)))
+saveRDS(models, "data/models.rds")
+
+# Plot the perplexity
+perps <- sapply(models, perplexity, dtm_test)
+png(filename = "data/perplexity.png")
+plot(ks, perps, xlab = "Number of topics", ylab = "Perplexity")
+dev.off()
