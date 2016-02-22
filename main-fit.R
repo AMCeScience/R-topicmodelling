679e4731b419236c6c76b06299744e114b159d40
Allard van Altena <flythe1@gmail.com>
2016-02-22 16:27:56 +0100

    update

--- main-fit.R
+++ main-fit.R
@@ -1,20 +1,3 @@
-# Clear workspace
-rm(list = ls())
-
-workspace <- "~/workspace/R"
-
-# Set workspace to folder where articles.csv is placed
-setwd(workspace)
-
-# Load the config
-if (!exists("configLoaded")) source("config.R")
-
-# if (file.exists("data/clean_corpus.rds")) cleanCorpus <- readRDS("data/clean_corpus.rds")
-if (!exists("cleanCorpus")) {
-  source("preprocessing.R")
-  cleanCorpus <- runPreprocessing(CSVfileName, store = TRUE)
-}
-
 source("fit.R")
 
 run1 <- TmLDASimulation(cleanCorpus, 4, alpha, beta, iter, iter, keep)
