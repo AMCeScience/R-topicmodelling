d82356b473b5994539379ce357aa242b445d2ae4
Allard van Altena <flythe1@gmail.com>
2016-02-22 16:30:06 +0100

    added cleaning script

---
+++ main-clean.R
@@ -0,0 +1,16 @@
+# Clear workspace
+rm(list = ls())
+
+workspace <- "~/workspace/R"
+
+# Set workspace to folder where articles.csv is placed
+setwd(workspace)
+
+# Load the config
+if (!exists("configLoaded")) source("config.R")
+
+# if (file.exists("data/clean_corpus.rds")) cleanCorpus <- readRDS("data/clean_corpus.rds")
+if (!exists("cleanCorpus")) {
+  source("preprocessing.R")
+  cleanCorpus <- runPreprocessing(CSVfileName, store = TRUE)
+}
