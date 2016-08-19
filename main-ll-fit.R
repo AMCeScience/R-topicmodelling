ff2c2bb968efe621970439520b771738424e10c1
Allard van Altena <flythe1@gmail.com>
2016-08-19 13:25:38 +0200

    getting ready for new run

--- main-ll-fit.R
+++ main-ll-fit.R
@@ -64,89 +64,68 @@ timer <- proc.time()
 #for (k in seq(5, 5, 5)) {
 
 data <- mclapply(ks, function(k) TmLDASimulation(cleanCorpus, "", k, 50/k, 0.01, burnin, iter, thin, keep, store = FALSE, multiple = TRUE), mc.cores = cores, mc.silent = TRUE)  
-  
-saveRDS(data, paste(storeFolder, "TM_LDA_LL_FULL_FIT.rds", sep = "/"))
-#   holder <- list()
-#   
-#   for(item in data) {
-#     q <- item@fitted[[1]]@logLiks
-#     holder[[length(holder) + 1]] <- q
-#   }
-#   
-#   mcmc_list <- list()
-#   
-#   for(item in data) {
-#     q <- item@fitted[[1]]@logLiks
-#     mcmc_list[[length(mcmc_list) + 1]] <- mcmc(q[floor(1/2 * length(q)):length(q)])
-#   }
-#   
-#   gelman.diag(mcmc_list)
-#   
-#   store_list[[length(store_list) + 1]] <- data
-# 
-#   saveRDS(data, gsub("__", k, paste("data", storeFolder, "TM_LDA_MCMC_LL_num__.rds", sep = "/")))
-#}
-#
-#saveRDS(store_list, paste("data", storeFolder, "TM_LDA_MCMC_LL.rds", sep = "/"))
-#
-# for (data in store_list) {
-#   mcmc_list <- list()
-#   
-#   for(item in data) {
-#     mcmc_list[[length(mcmc_list) + 1]] <- mcmc(item[floor(1/2 * length(item)):length(item)])
-#   }
-#   
-#   data_list <- list()
-#   
-#   for(item in data) {
-#     data_list[[length(data_list) + 1]] <- matrix(item[floor(1/2 * length(item)):length(item)], nrow=1)
-#   }
-#   
-#   plot_list <- list()
-#   
-#   for(item in data) {
-#     q <- item@fitted[[1]]@logLiks
-#     plot_list[[length(plot_list) + 1]] <- q
-#   }
-#   
-#   plot(unlist(plot_list), type="n", xlim=c(1,length(plot_list[[1]])))
-#   mapply(lines, plot_list, col = seq_along(plot_list), lty=2)
-#   
-#   print(gelman.diag(mcmc_list))
-#   
-#   quit
-# }
-#
 
-# highest_lls <- list()
-# 
-# for (ll_lists in data) {
-#   all_lls <- list()
-#   
-#   for (ll_list in ll_lists) {
-#     ll_list <- ll_list@fitted[[1]]@logLiks
-#     
-#     all_lls <- c(all_lls, tail(ll_list, n = 1))
-#   }
-#   
-#   highest_lls <- c(highest_lls, max(ll_list))
-# }
-# 
-# saveRDS(highest_lls, paste("data", storeFolder, "TM_LDA_MCMC_LL.rds", sep = "/"))
-# 
-# data1 <- readRDS("data/TM_LDA_MCMC_LL.rds")
-# data2 <- readRDS("data/bla/TM_LDA_MCMC_LL.rds")
-# data3 <- readRDS("data/bla1/TM_LDA_MCMC_LL.rds")
-# data4 <- readRDS("data/bla2/TM_LDA_MCMC_LL.rds")
-# 
-# data <- c(data1, data2, data3, data4)
-# 
+saveRDS(data, paste(storeFolder, "TM_LDA_LL_FULL_FIT.rds", sep = "/"))
+
+store_list <- list()
+highest_lls <- list()
+
+for (models in data) {
+  mcmc_list <- list()
+  plot_list <- list()
+  highest_ll <- 0
+  
+  for(model in models) {
+    q <- model@fitted[[1]]@logLiks
+    
+    mcmc_list[[length(mcmc_list) + 1]] <- mcmc(q[floor(1/2 * length(q)):length(q)])
+    plot_list[[length(plot_list) + 1]] <- q
+    
+    last_ll <- tail(q, n = 1)
+    
+    if (last_ll * -1 > highest_ll) {
+      highest_ll <- last_ll
+    }
+  }
+  
+  #plot(unlist(plot_list), type="n", xlim=c(1,length(plot_list[[1]])))
+  #mapply(lines, plot_list, col = seq_along(plot_list), lty=2)
+  
+  #print(data[[1]]@fitted[[1]]@k)
+  #print(gelman.diag(mcmc_list))
+  
+  store_list[[length(store_list) + 1]] <- plot_list
+  highest_lls <- c(highest_lls, highest_ll)
+}
+
+saveRDS(store_list, "data/TM_LDA_LL.rds")
+saveRDS(highest_lls, "data/TM_LDA_HIGHEST_LL.rds")
+
+#data1 <- unlist(readRDS("data/tests/1TM_LDA_MCMC_LL.rds")) # 5 - 100, 5
+#data2 <- unlist(readRDS("data/tests/2TM_LDA_MCMC_LL.rds")) # 150 - 500, 50
+#data3 <- unlist(readRDS("data/tests/3TM_LDA_MCMC_LL.rds")) # 600 - 1000, 100
+
+#data <- c(data1,data2,data3)
+
 # y <- c(seq(5,100,5), seq(110,180,10), seq(200,400,50), seq(500,700,100))
+#y <- c(seq(5,100,5), seq(150,500,50), seq(600,1000,100))
+
+# bla <- data - ((y/2) * 1308)
+# bla <- (2 * y) - (2 * data)
+# 
+# d<-t(rbind(y, bla))
+# plot(d, type="o", xlab="", ylab="", xlim=c(0,1005))
+# title(xlab = "Number of Topics", line = 2.2, cex.lab = 1)
+# title(ylab = "BIC", line = 2.3, cex.lab = 1)
+# 
+# data <- c(data4)
+# 
+# y <- c(seq(200,300,5))
 # 
 # d<-t(rbind(y, data))
-# plot(d, type="o", xlab="", ylab="", xlim=c(0,700))
+# plot(d, type="o", xlab="", ylab="", xlim=c(195,305))
 # title(xlab = "Number of Topics", line = 2.2, cex.lab = 1)
 # title(ylab = "Estimated Log Likelihood", line = 2.3, cex.lab = 1)
-# 
-# print("Ending run.")
-# print(proc.time() - timer)
+
+print("Ending run.")
+print(proc.time() - timer)
