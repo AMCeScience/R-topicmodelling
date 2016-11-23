5ab0acd9726467fa5d20e1abdce8d88904f3bc20
Allard van Altena <flythe1@gmail.com>
2016-11-23 18:51:51 +0100

    fix folder names

--- main-fit-and-tree.R
+++ main-fit-and-tree.R
@@ -27,7 +27,7 @@ if (length(args) > 0) {
   ks = c(20, 30, 40, 50, 75, 100)
   cores <- 7
   
-  versions <- list(c('stemmed-grammed', TRUE, TRUE), c('stemmed-notgrammed', TRUE, FALSE), c('notstemmed-grammed', FALSE, TRUE))
+  versions <- list(c('stemmed_grammed', TRUE, TRUE), c('stemmed_notgrammed', TRUE, FALSE), c('notstemmed_grammed', FALSE, TRUE))
   
   #datasets <- c(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 150, 200, 250, 300, 350, 400, 450, 500)
   datasets <- c(20, 25, 30, 40, 50, 75, 100)
