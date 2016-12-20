4e69a4a0091c5e6ecacf552ad529dceac02890ba
Allard van Altena <flythe1@gmail.com>
2016-12-20 11:26:29 +0100

    add CLI execution header

--- main-random-forest.R
+++ main-random-forest.R
@@ -1,3 +1,9 @@
+#!/usr/bin/env Rscript
+# https://stackoverflow.com/questions/18306362/run-r-script-from-command-line
+
+# Clear workspace
+rm(list = ls())
+
 args <- commandArgs(trailingOnly = TRUE)
 
 source("includes/contrast.R")
