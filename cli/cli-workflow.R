ea7fbccd8311fd4b585d3b9a88e883341ac8d86b
Allard van Altena <flythe1@gmail.com>
2017-05-03 14:25:25 +0200

    change workflow to provide dtm instead of corpus to lda fitting

--- cli/cli-workflow.R
+++ cli/cli-workflow.R
@@ -24,6 +24,8 @@ source("libraries/preprocessing.R")
 # Pre-process the corpus
 clean_corpus <- setupPreprocessing(project_name, csv_name)
 
+dtm <- DocumentTermMatrix(clean_corpus)
+
 if (workflow_run_to == "cleaning") {
   stop()
 }
@@ -43,7 +45,7 @@ library(parallel)
 if (fit_parallel) {
   datasets <- mclapply(
     fit_ks,
-    function(k) setupFitting(clean_corpus, project_name, file_version, k, fit_divider/k, fit_beta, fit_burnin, fit_iter, fit_thin, fit_keep),
+    function(k) setupFitting(dtm, project_name, file_version, k, fit_divider/k, fit_beta, fit_burnin, fit_iter, fit_thin, fit_keep),
     mc.cores = parallel_cores,
     mc.silent = parallel_silent
   )
@@ -51,7 +53,7 @@ if (fit_parallel) {
   datasets <- list()
 
   for (k in fit_ks) {
-    datasets <- append(datasets, list(setupFitting(clean_corpus, project_name, file_version, k, fit_divider/k, fit_beta, fit_burnin, fit_iter, fit_thin, fit_keep)))
+    datasets <- append(datasets, list(setupFitting(dtm, project_name, file_version, k, fit_divider/k, fit_beta, fit_burnin, fit_iter, fit_thin, fit_keep)))
   }
 }
 
